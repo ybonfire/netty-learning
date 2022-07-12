5c01f9efd4626aeba74b5266f522befbf34833de
Bo.Yuan5 <Bo.Yuan5@lotuscars.com.cn>
2022-07-12 18:38:59 +0800

    feat:新增 实现滑动窗口限流器、令牌桶限流器

--- pipeline-common/src/main/java/org/ybonfire/pipeline/common/ratelimiter/IRateLimiter.java
+++ pipeline-common/src/main/java/org/ybonfire/pipeline/common/ratelimiter/IRateLimiter.java
@@ -7,5 +7,12 @@ package org.ybonfire.pipeline.common.ratelimiter;
  * @date 2022-07-11 18:17
  */
 public interface IRateLimiter {
+
+    /**
+     * @description: 获取通过权限
+     * @param:
+     * @return:
+     * @date: 2022/07/12 16:34:53
+     */
     boolean acquire(final int acquires);
 }
--- pipeline-common/src/main/java/org/ybonfire/pipeline/common/ratelimiter/impl/SlidingWindowRateLimiter.java
+++ pipeline-common/src/main/java/org/ybonfire/pipeline/common/ratelimiter/impl/SlidingWindowRateLimiter.java
@@ -1,5 +1,7 @@
 package org.ybonfire.pipeline.common.ratelimiter.impl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.ybonfire.pipeline.common.exception.ExceptionTypeEnum;
@@ -9,22 +11,31 @@ import org.ybonfire.pipeline.common.util.ExceptionUtil;
 import lombok.Builder;
 
 /**
- * 固定窗口限流器
+ * 滑动窗口限流器
  *
  * @author Bo.Yuan5
- * @date 2022-07-11 18:29
+ * @date 2022-07-12 14:42
  */
-public final class FixedWindowRateLimiter implements IRateLimiter {
-    private final Window window;
+public final class SlidingWindowRateLimiter implements IRateLimiter {
+    private static final int WINDOW_COUNT = 10;
+    private final List<Window> windows = new ArrayList<>();
+    private final long intervalMillisPerWindow;
+    private final long acquiresPerWindow;
 
-    public FixedWindowRateLimiter(final long intervalMillis, final long acquires) {
+    public SlidingWindowRateLimiter(final long intervalMillis, final long acquires) {
         if (intervalMillis < 1000L) {
             throw ExceptionUtil.exception(ExceptionTypeEnum.ILLEGAL_ARGUMENT);
         }
         if (acquires < 0) {
             throw ExceptionUtil.exception(ExceptionTypeEnum.ILLEGAL_ARGUMENT);
         }
-        this.window = Window.builder().intervalMillis(intervalMillis).acquires(acquires).build();
+
+        this.intervalMillisPerWindow = intervalMillis / WINDOW_COUNT;
+        this.acquiresPerWindow = acquires / WINDOW_COUNT;
+
+        for (int i = 0; i < WINDOW_COUNT; ++i) {
+            windows.add(Window.builder().intervalMillis(intervalMillisPerWindow).acquires(acquiresPerWindow).build());
+        }
     }
 
     /**
@@ -38,7 +49,18 @@ public final class FixedWindowRateLimiter implements IRateLimiter {
         if (acquires < 0) {
             throw ExceptionUtil.exception(ExceptionTypeEnum.ILLEGAL_ARGUMENT);
         }
-        return window.acquire(acquires);
+        return currentWindow().acquire(acquires);
+    }
+
+    /**
+     * @description: 获取当前时间窗口
+     * @param:
+     * @return:
+     * @date: 2022/07/12 17:56:05
+     */
+    private Window currentWindow() {
+        final int index = ((int)(System.currentTimeMillis() / intervalMillisPerWindow)) % this.windows.size();
+        return this.windows.get(index);
     }
 
     /**
@@ -59,7 +81,7 @@ public final class FixedWindowRateLimiter implements IRateLimiter {
          * @return:
          * @date: 2022/07/12 16:34:53
          */
-        public boolean acquire(final long acquires) {
+        public synchronized boolean acquire(final long acquires) {
             tryRefresh();
             return counter.getAndAdd(acquires) <= this.acquires;
         }
